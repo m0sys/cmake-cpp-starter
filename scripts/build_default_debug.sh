9062a3092215ef16c23363464df70cb29091a1d9
m0sys <21181457+m0sys@users.noreply.github.com>
2022-07-08 11:47:02 -0400

    feat(cmake): add modular project struct with working catch2!!! 🔥🔥🔥

--- scripts/build_default_debug.sh
+++ scripts/build_default_debug.sh
@@ -3,4 +3,4 @@
 # Config opt will only work for multi-config generators such as XCode.
 # Use the ninja generator for single-config gen alt - cross platform.
 cmake -S . -B build/build_debug
-cmake --build build/build_debug  --config Debug --target my_app_debug
+cmake --build build/build_debug  --config Debug --target cmake_cpp_starter_my_app cmake_cpp_starter_my_app_test
--- scripts/build_default_release.sh
+++ scripts/build_default_release.sh
@@ -3,4 +3,4 @@
 # Config opt will only work for multi-config generators such as XCode.
 # Use the ninja generator for single-config gen alt - cross platform.
 cmake -S . -B build/build_release
-cmake --build build/build_release  --config Release --target my_app
+cmake --build build/build_release  --config Release --target cmake_cpp_starter_my_app
