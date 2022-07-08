3ea799c1be81856ab421e7496e7cc9b444e0d905
m0sys <21181457+m0sys@users.noreply.github.com>
2022-07-08 15:05:07 -0400

    feat(cmake): add some fmt and spdlog fun to the mix

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,12 @@
 #include "coffee/coffee.h"
+#include "spdlog/spdlog.h"
+#include <fmt/core.h>
 #include <iostream>
 
 int main()
 {
     std::cout << "Hello, World!\n";
     coffee();
+    fmt::print("Hello, World from FMT!\n");
+    spdlog::info("Welcome from SPDLOG");
 }
